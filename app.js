afbca9bf51f2ecae4f1a0c3bd5f20e95348ff16c
imdounia <douniabakhkhouch2000@gmail.com>
2024-11-29 12:12:57 +0100

    handle CORS errors

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE');
+        return res.status(200);
+    }
+    next();
+})
+
 //routes which should handle the requests
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes);
